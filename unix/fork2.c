4c6dd0adf225e840475ec2be887c8b51f77d4e32
ryoshio- <ryoshio-@student.42sp.org.br>
2022-04-20 00:14:01 +0200

    minitalk

--- unix/fork2.c
+++ unix/fork2.c
@@ -6,7 +6,7 @@
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/12 02:25:43 by ryoshio-          #+#    #+#             */
-/*   Updated: 2022/04/12 02:54:33 by ryoshio-         ###   ########.fr       */
+/*   Updated: 2022/04/12 03:17:33 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,20 @@ int main(void)
 {
 	fork();
 	fork();
-	fork();
-	fork();
+	
 	printf("Quantas vezes eu vou aparecer?\n");
+	return(0);
 }
+
+/*
+Parece que só  é possivel numero para de fork
+Analisar com calma
+
+fork |printf
+1    | 2
+2	 | 4
+3    | da erro
+4    |7
+
+
+*/
--- unix/fork4.c
+++ unix/fork4.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fork1.c                                            :+:      :+:    :+:   */
+/*   fork4.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ryoshio- <ryoshio-@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/04/12 02:19:41 by ryoshio-          #+#    #+#             */
-/*   Updated: 2022/04/12 03:09:27 by ryoshio-         ###   ########.fr       */
+/*   Created: 2022/04/12 03:03:00 by ryoshio-          #+#    #+#             */
+/*   Updated: 2022/04/12 03:23:40 by ryoshio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,21 @@
 int main (int argc, char* argv[])
 {
     int id = fork();
-    printf("id: %d\n", id);
-    return (0);
+    
+    if (id != 0)
+	{
+		printf("Entrei no if\n");
+		fork();
+		printf("depois do fork do if\n");
+	}
+    printf("Adivinha o que vai acontecer? \n");
+    return(0);
 }
-
 /*
-Fork seria tipo uma bifurcação, por isso que o printf é exercutado duas vezes. 
-Repare que o valor do id muda 
+primeiro fork vai ter duas bivorcação 
+1º vai entrar no if ( vai causa "Entei no if")
+2º Vai seguir reto  (vai causa "depois do fork")
+
+Segundo fork vai causa os restante 
+
 */
